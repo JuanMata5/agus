a9af163238d6531054fc6be71ebe815d6f9b0724
JuanMata5 <juanjitus12@gmail.com>
2026-04-29 14:00:38 -0300

    Fix: favicon.ico 500 error

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 import express from "express";
 import ipHandler from "./api/ip.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(express.json());
 
+// Servir archivos estáticos (incluye favicon.ico)
+app.use(express.static(path.join(__dirname, "public")));
+
+// Ruta específica para favicon.ico para evitar 500
+app.get("/favicon.ico", (req, res) => {
+  res.status(204).end();
+});
+
 app.use("/api/ip", ipHandler);
 
 app.get("/", (req, res) => {
-  res.send("API funcionando ✔");
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 export default app;
